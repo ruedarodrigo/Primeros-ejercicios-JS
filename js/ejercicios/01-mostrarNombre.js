ffca5241b40522d035aef36c4ac1341bf5816d0b
Rodrigo Rueda <rodrigo21492@gmail.com>
2026-03-03 16:18:51 -0300

    agregue un ejemplo de como declarar variables y los tipos de datos

---
+++ js/ejercicios/01-mostrarNombre.js
@@ -0,0 +1,11 @@
+//como pedir un dato al usuario y mostrarlo por pantalla
+const nombre = prompt("ingresa tu nombre")
+const apellido = prompt("ingresa tu apellido")
+
+console.log(nombre)
+console.log(apellido)
+
+//mostrar un mensaje al usuario
+//alert("Tu nombre y apellido es: " + nombre + ", " + apellido)
+
+document.writeln()
